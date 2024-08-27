1f222c8991b42269aa6b962a9f0457f3616b1e05
Tsunoka(Hosoya).Shiori <petit0426@icloud.com>
2024-08-27 22:50:43 +0900

    ヘッダー・ドロワーメニューCSS・JS（SP）作成

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
 // lp1_js
 
+//ハンバーガーボタン
+var hamburger = $('.drawer-menu-container');
+// OPEN/CLOSEボタンをクリックしたら
+$('.hamburger-button').on('click', function () {
+    console.log('click');
+    // .hamburgerの表示・非表示を繰り返す
+    hamburger.toggleClass('drawer-menu-active');
+});
+// 画面幅のサイズが変わったら
+$(window).on('resize', function () {
+    // console.log('resize');
+    // ハンバーガーメニューを閉じる
+    hamburger.removeClass('drawer-menu-active');
+});
+
 //スムーススクロール　参考：https://125naroom.com/web/2899
